7a243a330711842285a7091be0a938b4350464ca
Eleke Great Chima <59459106+Great187520@users.noreply.github.com>
2023-11-20 21:53:08 +0100

    Add files via upload

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,24 +1,23 @@
 // Example weather service utilizing weatherstack.com API (assuming an API key is used)
-const API_KEY = 'a564af7a34dd71288baa35a7ae07714c';
+const API_KEY = "a564af7a34dd71288baa35a7ae07714c";
 
-const weatherService = {
-  //async fetchWeatherForCity(cityName) {
-  export const fetchWeatherForCity = async (cityName) => {
-    try {
-      const response = await fetch(`http://api.weatherstack.com/current?access_key=${API_KEY}&query=${cityName}`);
-      if (!response.ok) {
-        throw new Error('Weather data not available');
-      }
-      const data = await response.json();
-      return {
-        name: data.location.name,
-        temperature: data.current.temperature,
-        // Other weather details as needed
-      };
-    } catch (error) {
-      throw new Error('Failed to fetch weather data');
+export const fetchWeatherForCity = async (cityName) => {
+  try {
+    const response = await fetch(
+      `http://api.weatherstack.com/current?access_key=${API_KEY}&query=${cityName}`,
+    );
+    if (!response.ok) {
+      throw new Error("Weather data not available");
     }
-  },
+    const data = await response.json();
+    return {
+      name: data.location.name,
+      temperature: data.current.temperature,
+      // Other weather details as needed
+    };
+  } catch (error) {
+    throw new Error("Failed to fetch weather data");
+  }
 };
 
 //Example structure for weatherService.js
@@ -31,6 +30,3 @@ const weatherService = {
     throw new Error('Failed to fetch weather data');
   }
 };*/
-
-
-export default weatherService;
